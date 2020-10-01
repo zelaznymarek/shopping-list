e92e06d26bdf0fb0f7844eb43470f57d262ed36d
Marek <zelaznymarek@gmail.com>
2020-10-01 21:57:07 +0200

    implement crud, add crud tests

--- backend/app/endpoints/category.py
+++ backend/app/endpoints/category.py
@@ -7,17 +7,26 @@ from app.auth import get_current_user
 from app.db.session import get_db
 from app.db import models
 from app.schemas import category as schemas
+from app.crud import category as crud
 
 router = APIRouter()
 
 
-@router.get('/{category_id}', response_model=schemas.Category)
+@router.get('/{category_id}', response_model=schemas.Category, responses={status.HTTP_404_NOT_FOUND: {}})
 def get_one(
         category_id: int,
         current_user: models.User = Depends(get_current_user),
         db_session: Session = Depends(get_db)
 ) -> schemas.Category:
-    return crud.get_by_id(db_session, category_id)
+    db_category = crud.get_by_id(db_session, category_id)
+
+    if not db_category:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'The category with id "{category_id}" not found.'
+        )
+
+    return db_category
 
 
 @router.get('/', response_model=List[schemas.Category])
@@ -25,7 +34,7 @@ def get_all(current_user: models.User = Depends(get_current_user), db_session: S
     return crud.get_list(db_session)
 
 
-@router.post('/', response_model=schemas.Category)
+@router.post('/', response_model=schemas.Category, responses={status.HTTP_400_BAD_REQUEST: {}})
 def add(
         new_category: schemas.CategoryCreate,
         current_user: models.User = Depends(get_current_user),
@@ -39,28 +48,12 @@ def add(
             detail='The category with this name already exists in the system.'
         )
 
-    return crud.create(db_session, new_category)
+    return crud.create(db_session, new_category.dict())
 
 
 @router.delete('/{category_id}')
 def remove(
         category_id: int,
-        current_user: models.User = Depends(get_current_user),
-        db_session: Session = Depends(get_db)
-):
-    if not crud.get_by_id(db_session, category_id):
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f'The category with id "{category_id}" not found.'
-        )
-
-    return crud.remove_by_id(db_session, category_id)
-
-
-@router.put('/{category_id}', response_model=schemas.Category)
-def update(
-        category_id: int,
-        category_updated: schemas.CategoryCreate,
         current_user: models.User = Depends(get_current_user),
         db_session: Session = Depends(get_db)
 ):
@@ -72,4 +65,22 @@ def update(
             detail=f'The category with id "{category_id}" not found.'
         )
 
-    crud.update(db_session, db_category, category_updated)
+    return crud.remove_by_id(db_session, db_category)
+
+
+@router.put('/{category_id}', response_model=schemas.Category)
+def update(
+        category_id: int,
+        category_to_update: schemas.CategoryCreate,
+        current_user: models.User = Depends(get_current_user),
+        db_session: Session = Depends(get_db)
+):
+    db_category = crud.get_by_id(db_session, category_id)
+
+    if not db_category:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'The category with id "{category_id}" not found.'
+        )
+
+    return crud.update(db_session, db_category=db_category, category_to_update=category_to_update.dict())
